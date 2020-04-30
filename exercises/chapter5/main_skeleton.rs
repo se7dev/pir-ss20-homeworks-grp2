a8ea299996ea7bafeddc5f885cb75159a722d419
RoWe90 <robin.weisshaar@gmail.com>
2020-04-30 11:29:45 +0200

    added skeleton file

---
+++ exercises/chapter5/main_skeleton.rs
@@ -0,0 +1,72 @@
+// Define a Hero Struct
+#[derive(...)]
+struct Hero {
+
+}
+// Let Hero spawn, attack and move
+impl Hero {
+    // fn spawn(...) -> ... {
+    //
+    // }
+    // fn attack(...) {
+    //     ...
+    // }
+    // fn move_hero(...) {
+    //     ...
+    // }
+}
+
+// Define a Monster Struct
+#[derive(...)]
+struct Monster {
+
+}
+// Let Monster spawn, attack and move
+impl Monster {
+    // fn spawn(...) -> ... {
+    //     ...
+    // }
+    //
+    // fn attack(...) {
+    //     ...
+    // }
+    // fn move_monster(...) {
+    //    ...
+    // }
+    // // Make monster cloneable
+    // fn clone(...) {
+    //     ...
+    // }
+}
+
+#[derive(..., ...)]
+struct Gamefield {
+
+}
+
+impl Gamefield {
+    // fn create(...) -> ... {
+    //    ...
+    // }
+    // fn display(...) {
+    //     ...
+    // }
+}
+
+
+fn main() {
+    // Create Gamefield
+    // let mut gamefield = ...;
+    // Spawn a hero
+    // let mut hero = ...
+
+    // Spawn a Monster
+    // let mut monster = ...
+
+    // Let them fight, and clone a monster
+    // hero.attack(...);
+    // monster_1.attack(...);
+    // Move hero and mosnter
+    // hero.move_hero(...);
+    // monster_1.move_monster(...);
+}
